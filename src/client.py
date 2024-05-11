0b0fdd69b68d355bfa8205046df39af4fed6bdf5
Chinosu <46995931+Chinosu@users.noreply.github.com>
2024-05-12 02:51:33 +1000

    Version 0.1.0

--- src/client.py
+++ src/client.py
@@ -1,21 +1,80 @@
-import socket
-import getpass
+if __name__ != "__main__":
+    raise ImportError("This module is not meant to be imported")
+
+import asyncio
+import logging
+import random
+import sys
+from signal import SIGINT, SIGTERM
+
 from constants import *
 
-client_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-client_socket.connect(SOCKET_PATH)
 
-try:
-    message = f"Hello, UDS from {getpass.getuser()}!"
-    print(f"Sending: {message}")
-    client_socket.sendall(message.encode())
+# import getpass
+# username = getpass.getuser()
 
-    amount_received = 0
-    amount_expected = len(message)
 
-    while amount_received < amount_expected:
-        data = client_socket.recv(16)
-        amount_received += len(data)
-        print(f"Received: {data.decode()}")
-finally:
-    client_socket.close()
+async def connect_stdin_stdout():
+    loop = asyncio.get_event_loop()
+    reader = asyncio.StreamReader()
+    protocol = asyncio.StreamReaderProtocol(reader)
+
+    await loop.connect_read_pipe(lambda: protocol, sys.stdin)
+
+    w_transport, w_protocol = await loop.connect_write_pipe(
+        asyncio.streams.FlowControlMixin, sys.stdout
+    )
+    writer = asyncio.StreamWriter(w_transport, w_protocol, reader, loop)
+
+    return reader, writer
+
+
+async def handle_server(stdout, reader):
+    while True:
+        data = await reader.read(MAX_MESSAGE_SIZE)
+        if not data:
+            break
+        stdout.write(f"{data.decode()}\n".encode())
+        await stdout.drain()
+
+
+async def handle_user(stdin, username, writer):
+    while True:
+        message = await stdin.read(MAX_MESSAGE_SIZE)
+        writer.write(f"<{username}> {message.decode().strip()}".encode())
+        await writer.drain()
+
+
+async def main():
+    logging.basicConfig(level=logging.DEBUG)
+    username = str(random.randrange(1000))
+    stdin, stdout = await connect_stdin_stdout()
+    try:
+        reader, writer = await asyncio.open_unix_connection(SOCKET_PATH)
+        writer.write(f"{username} has joined the chat".encode())
+        await writer.drain()
+
+        tasks = map(
+            asyncio.create_task,
+            [handle_server(stdout, reader), handle_user(stdin, username, writer)],
+        )
+
+        done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+
+        for task in pending:
+            task.cancel()
+        for task in done:
+            task.result()
+    except ConnectionRefusedError:
+        print("Server unreachable.")
+
+    except asyncio.CancelledError:
+        for task in tasks:
+            task.cancel()
+        writer.write(f"{username} has left the chat".encode())
+        await writer.drain()
+        writer.close()
+        await writer.wait_closed()
+
+
+asyncio.run(main())
--- src/server.py
+++ src/server.py
@@ -1,27 +1,70 @@
-import socket
+if __name__ != "__main__":
+    raise ImportError("This module is not meant to be imported")
+
 import os
 import asyncio
+from signal import SIGINT, SIGTERM
+import logging
+import functools
+
 from constants import *
 
-if os.path.exists(SOCKET_PATH):
-    os.remove(SOCKET_PATH)
 
-server_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-server_socket.bind(SOCKET_PATH)
-server_socket.listen()
+async def handle_client(clients, reader, writer):
+    clients.add(writer)
 
-print(f"Server listening on {SOCKET_PATH}")
-
-while True:
-    connection, client_address = server_socket.accept()
     try:
-        print("Connection from", client_address)
         while True:
-            data = connection.recv(16)
-            if data:
-                print("Received:", data.decode())
-                connection.sendall(data)
-            else:
+            data = await reader.read(MAX_MESSAGE_SIZE)
+            if not data:
                 break
-    finally:
-        connection.close()
+
+            message = data.decode()
+            logging.info(f"Received: {message}")
+
+            for client in clients.copy():
+                try:
+                    client.write(data)
+                except (BrokenPipeError, ConnectionResetError) as e:
+                    clients.discard(client)
+    except BrokenPipeError:
+        clients.discard(writer)
+
+    try:
+        writer.close()
+        await writer.wait_closed()
+    except BrokenPipeError:
+        pass
+
+
+async def cleanup(server, clients):
+    for client in clients.copy():
+        client.close()
+        await client.wait_closed()
+    server.close()
+
+
+async def main():
+    logging.basicConfig(level=logging.DEBUG)
+    if os.path.exists(SOCKET_PATH):
+        os.remove(SOCKET_PATH)
+
+    clients = set()
+
+    server = await asyncio.start_unix_server(
+        functools.partial(handle_client, clients), path=SOCKET_PATH
+    )
+
+    for signal in (SIGINT, SIGTERM):
+        asyncio.get_event_loop().add_signal_handler(
+            signal, lambda: asyncio.ensure_future(cleanup(server, clients))
+        )
+
+    try:
+        async with server:
+            await server.serve_forever()
+    except asyncio.CancelledError:
+        pass
+
+
+asyncio.run(main())
