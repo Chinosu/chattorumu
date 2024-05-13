9e55f50934e097fccfaab4bb7594c30a72389b24
Chinosu <46995931+Chinosu@users.noreply.github.com>
2024-05-14 01:01:25 +1000

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,7 +1,3 @@
-from pathlib import Path
-
-# project_root = Path(__file__).parent.parent
-project_root = Path("/import/kamen/6/z5488485/_/repos/chattorumu")
-LOG_PATH = str(project_root / "server.log")
+LOG_PATH = "/import/kamen/6/z5488485/Public/chattorumu.src/server.log"
 SOCKET_PATH = "/import/kamen/6/z5488485/Public/chattorumu.sock"
 MESSAGE_SIZE = 280
