cd5a10343df192059bd2e1ea13330fc5cd69c1b6
Chinosu <46995931+Chinosu@users.noreply.github.com>
2024-05-14 03:39:11 +1000

    Update server public IP address

--- src/config.py
+++ src/config.py
@@ -3,5 +3,5 @@ LOG_PATH = "/import/kamen/6/z5488485/Public/chattorumu.src/server.log"
 MESSAGE_SIZE = 280
 
 SERVER_HOST = "0.0.0.0"
-CLIENT_HOST = "129.94.242.93"
+CLIENT_HOST = "129.94.242.92"
 PORT = 25565
